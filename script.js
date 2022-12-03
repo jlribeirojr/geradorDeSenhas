b8fae47b7b3ee4c3bb772ca83a5e3e0182c02b02
jlribeirojr <107446939+jlribeirojr@users.noreply.github.com>
2022-12-02 23:48:51 -0300

    codando a parte funcional

---
+++ script.js
@@ -0,0 +1,10 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+// a b c d e f g h i j k l m n o p q r s t u v w x y z
+// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+// 0123456789
